5924a9e85ce5f3497f4d83cb0f64a5b372ecfd4b
Agasteen-ajs <agasteen.96@gmail.com>
2020-12-11 10:49:06 +0530

    sum and product

---
+++ sum/sum.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5];
+let sum = 0, product = 1;
+for (let i = 0; i < arr.length; i++)
+{
+    sum += arr[i];
+    product *= arr[i];
+}
+console.log('Sum = '+ sum + '\t Product = ' + product )
